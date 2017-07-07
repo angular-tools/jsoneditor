23b799ca268f2beb71c882c0040e12bffdc7c3b0
Frédéric SICRE <frederic.sicre@educlever.com>
2017-07-07 17:12:13 +0200

    protection against missing $scope.options

--- ng-jsoneditor.js
+++ ng-jsoneditor.js
@@ -49,28 +49,45 @@
                     var instance = new JSONEditor(element[0], theOptions);
 
                     if ($scope.ngJsoneditor instanceof Function) {
-                        $timeout(function () { $scope.ngJsoneditor(instance);});
+                        $timeout(function () {
+                            $scope.ngJsoneditor(instance);
+                        });
                     }
 
                     return instance;
                 }
 
                 $scope.$watch('options', function (newValue, oldValue) {
-                    for (var k in newValue) {
-                        if (newValue.hasOwnProperty(k)) {
-                            var v = newValue[k];
+                    if (editor) {
+                        for (var k in newValue) {
+                            if (newValue.hasOwnProperty(k)) {
+                                var v = newValue[k];
 
-                            if (newValue[k] !== oldValue[k]) {
-                                if (k === 'mode') {
-                                    editor.setMode(v);
-                                } else if (k === 'name') {
-                                    editor.setName(v);
-                                } else { //other settings cannot be changed without re-creating the JsonEditor
-                                    editor = _createEditor(newValue);
-                                    $scope.updateJsonEditor();
-                                    return;
+                                if (!oldValue || newValue[k] !== oldValue[k]) {
+                                    if (k === 'mode') {
+                                        editor.setMode(v);
+                                    } else if (k === 'name') {
+                                        editor.setName(v);
+                                    } else if (k === 'expanded') {
+                                        if (newValue[k]) {
+                                            editor.expandAll && editor.expandAll()
+                                        } else {
+                                            editor.collapseAll && editor.collapseAll()
+                                        }
+                                    } else { //other settings cannot be changed without re-creating the JsonEditor
+                                        editor = _createEditor(newValue);
+                                        $scope.updateJsonEditor();
+                                        return;
+                                    }
                                 }
                             }
+                        }
+                    } else {
+                        editor = _createEditor($scope.options);
+                        if ($scope.options) {
+                            editor.expandAll && editor.expandAll()
+                        } else {
+                            editor.collapseAll && editor.collapseAll()
                         }
                     }
                 }, true);
@@ -96,17 +113,13 @@
                         } else {
                             editor.set(ngModel.$viewValue || {});
                         }
-                    }, $scope.options.timeout || 100);
+                    }, ($scope.options && $scope.options.timeout) || 100);
                 };
 
-                editor = _createEditor($scope.options);
-
-                if ($scope.options.hasOwnProperty('expanded')) {
-                    $timeout($scope.options.expanded ? function () {editor.expandAll()} : function () {editor.collapseAll()}, ($scope.options.timeout || 100) + 100);
-                }
-
                 ngModel.$render = $scope.updateJsonEditor;
-                $scope.$watch(function () { return ngModel.$modelValue; }, $scope.updateJsonEditor, true); //if someone changes ng-model from outside
+                $scope.$watch(function () {
+                    return ngModel.$modelValue;
+                }, $scope.updateJsonEditor, true); //if someone changes ng-model from outside
             }
         };
     }]);
